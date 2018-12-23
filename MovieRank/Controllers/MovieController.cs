f76851f15590098e97a55061c8e486627df2dbe5
Johnny Olson <john.olson3@gmail.com>
2018-12-22 19:46:50 -0800

    Add dependancy

--- MovieRank/Controllers/MovieController.cs
+++ MovieRank/Controllers/MovieController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MovieRank.Services;
 
 namespace MovieRank.Controllers
 {
+    [Route("movies")]
     public class MovieController : Controller
     {
-        
+        private readonly IMovieRankService _movieRankService;
+
+        MovieController(IMovieRankService movieRankService)
+        {
+            _movieRankService = movieRankService;
+        }
     }
 }
--- MovieRank/Startup.cs
+++ MovieRank/Startup.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Amazon.DynamoDBv2;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Microsoft.Extensions.DependencyInjection;
+using MovieRank.Services;
 
 namespace MovieRank
 {
@@ -17,15 +13,15 @@ namespace MovieRank
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddAWSService<IAmazonDynamoDB>();
+            services.AddSingleton<IMovieRankService, MovieRankService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
             app.UseMvc();
         }
